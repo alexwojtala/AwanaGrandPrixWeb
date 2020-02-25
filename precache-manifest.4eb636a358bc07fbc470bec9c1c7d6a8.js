54db658c292fd71520eab6430bcdaaec6c80f6d9
Alex Wojtala <alexwojtala@gmail.com>
2020-02-25 00:00:43 -0500

    Updates

---
+++ precache-manifest.4eb636a358bc07fbc470bec9c1c7d6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3460b2f6b818ff8c77de08baebafce85",
+    "url": "/AwanaGrandPrixWeb/index.html"
+  },
+  {
+    "revision": "47def40ea8d2fa548137",
+    "url": "/AwanaGrandPrixWeb/static/css/main.80c55252.chunk.css"
+  },
+  {
+    "revision": "266a0a0c8c608b382b16",
+    "url": "/AwanaGrandPrixWeb/static/js/2.1bdcc914.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/AwanaGrandPrixWeb/static/js/2.1bdcc914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47def40ea8d2fa548137",
+    "url": "/AwanaGrandPrixWeb/static/js/main.1da00ddb.chunk.js"
+  },
+  {
+    "revision": "9a84d4525d278d90b3b4",
+    "url": "/AwanaGrandPrixWeb/static/js/runtime-main.3ca6abd2.js"
+  }
+]);
