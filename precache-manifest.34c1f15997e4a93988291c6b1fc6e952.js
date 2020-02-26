cc8ac31c2c5efc8873b79fc38bf5d6eae481f57f
Alex Wojtala <alexwojtala@gmail.com>
2020-02-25 21:22:54 -0500

    Updates

--- precache-manifest.34c1f15997e4a93988291c6b1fc6e952.js
+++ precache-manifest.34c1f15997e4a93988291c6b1fc6e952.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbdb3b0275abc1f0e8a0d3a0e8e20158",
+    "revision": "32513ae1e7680af926acfa6deccfb51b",
     "url": "/AwanaGrandPrixWeb/index.html"
   },
   {
-    "revision": "c4040e844bb3e12d1849",
+    "revision": "4a79f1a7836b1bb3593e",
     "url": "/AwanaGrandPrixWeb/static/css/main.10d7d995.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/AwanaGrandPrixWeb/static/js/2.1bdcc914.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4040e844bb3e12d1849",
-    "url": "/AwanaGrandPrixWeb/static/js/main.b51363a1.chunk.js"
+    "revision": "4a79f1a7836b1bb3593e",
+    "url": "/AwanaGrandPrixWeb/static/js/main.e9c5d60b.chunk.js"
   },
   {
     "revision": "9a84d4525d278d90b3b4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AwanaGrandPrixWeb/precache-manifest.98b6c96278426445c56ba917b5b96c20.js"
+  "/AwanaGrandPrixWeb/precache-manifest.34c1f15997e4a93988291c6b1fc6e952.js"
 );
 
 self.addEventListener('message', (event) => {
