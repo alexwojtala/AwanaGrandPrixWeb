7502e71eff7c1cdd7ce4c7a14ccf01e3ae19a5b9
Alex Wojtala <alexwojtala@gmail.com>
2020-11-07 23:05:12 -0500

    Fix lint warnings and error and format file

--- src/components/CheckInConfirmation/CheckInConfirmation.tsx
+++ src/components/CheckInConfirmation/CheckInConfirmation.tsx
@@ -1,21 +1,31 @@
-import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 interface CheckInConfrimationProps {
-    clubber: string | undefined,
-    groupId: string | undefined,
-    onContinue: Function
+  clubber: string | undefined;
+  groupId: string | undefined;
+  onContinue: () => void;
 }
 
-const CheckInConfirmation = ({clubber, groupId, onContinue}: CheckInConfrimationProps) => {
-    return (
-        <div className={'container'}>
-            <h2>you're entered <FontAwesomeIcon icon={faCheck} /></h2>
-            <div>{clubber} is <strong>#{groupId}</strong></div>
-            <button className={'enrollClubber'} onClick={() => onContinue()}>Enroll next clubber</button>
-        </div>
-    )
-}
+const CheckInConfirmation = ({
+  clubber,
+  groupId,
+  onContinue,
+}: CheckInConfrimationProps): JSX.Element => {
+  return (
+    <div className={"container"}>
+      <h2>
+        you are entered <FontAwesomeIcon icon={faCheck} />
+      </h2>
+      <div>
+        {clubber} is <strong>#{groupId}</strong>
+      </div>
+      <button className={"enrollClubber"} onClick={() => onContinue()}>
+        Enroll next clubber
+      </button>
+    </div>
+  );
+};
 
-export default CheckInConfirmation
+export default CheckInConfirmation;
