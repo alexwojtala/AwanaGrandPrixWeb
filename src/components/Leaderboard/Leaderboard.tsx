da257f3d8a93179eff19f8710c3fc4970acb2312
Alex Wojtala <alexwojtala@gmail.com>
2020-11-07 23:09:03 -0500

    Fix lint warnings and format file

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,39 +1,45 @@
-import React from "react"
+import React from "react";
 
-import "./Leaderboard.css"
+import "./Leaderboard.css";
+
+interface Leader {
+  group_id: string;
+  clubber: string;
+  points: number;
+}
 
 interface LeaderboardProps {
-    title: string,
-    leaders: any[] | undefined
+  title: string;
+  leaders: Leader[] | undefined;
 }
 
-const Leaderboard = ({title, leaders}: LeaderboardProps) => {
-    return (
-        <div className={'leaderboardContainer'}>
-            <h1>{title}</h1>
-            <table className={'leaderboardTable'}>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>NAME</th>
-                        <th>POINTS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { leaders !== undefined && leaders.map((leader: any, i: number) => {
-                        return (
-                            <tr key={'regular' + i}>
-                                <td>{leader.group_id}</td>
-                                <td>{leader.clubber}</td>
-                                <td>{leader.points}</td>
-                            </tr>
-                        )
-                    })
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
-}
+const Leaderboard = ({ title, leaders }: LeaderboardProps): JSX.Element => {
+  return (
+    <div className={"leaderboardContainer"}>
+      <h1>{title}</h1>
+      <table className={"leaderboardTable"}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>POINTS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaders !== undefined &&
+            leaders.map((leader: Leader, i: number) => {
+              return (
+                <tr key={"regular" + i}>
+                  <td>{leader.group_id}</td>
+                  <td>{leader.clubber}</td>
+                  <td>{leader.points}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
